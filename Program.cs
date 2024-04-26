bda4f0865d82b97ad19841f89275ad9700c8a011
Zavier Hopson <zo.hopson94@gmail.com>
2024-04-26 09:10:00 -0500

    initial

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using CarBuilder.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+List<PaintColor> paints = new List<PaintColor>()
+{
+    new PaintColor()
+    {
+
+    }
+}
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.Run();
+
+
