1125f8f32a06b578a7e1ebe7bd130601de706a67
Zavier Hopson <zo.hopson94@gmail.com>
2024-04-29 17:43:23 -0500

    created new model

---
+++ Models/DTOs/NewOrderDTO.cs
@@ -0,0 +1,9 @@
+namespace CarBuilder.Models.DTOs;
+
+public class NewOrderDTO
+{
+    public int WheelId { get; set; }
+    public int TechnologyId { get; set; }
+    public int PaintId { get; set; }
+    public int InteriorId { get; set; }
+}
