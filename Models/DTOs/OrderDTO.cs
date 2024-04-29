bb740f48a57decf35855f5f3fe44c7456effd6f3
Zavier Hopson <zo.hopson94@gmail.com>
2024-04-29 17:48:02 -0500

    updated properties

--- Models/DTOs/OrderDTO.cs
+++ Models/DTOs/OrderDTO.cs
@@ -5,7 +5,11 @@ public class OrderDTO
     public int Id { get; set; }
     public DateTime TimeStamp { get; set; }
     public int WheelId { get; set; }
+    public WheelDTO Wheel { get; set; }
     public int TechnologyId { get; set; }
-    public int PaintIt { get; set; }
+    public TechnologyDTO Technology { get; set; }
+    public int PaintId { get; set; }
+    public PaintColorDTO Paint { get; set; }
     public int InteriorId { get; set; }
+    public InteriorDTO Interior { get; set; }
 }
